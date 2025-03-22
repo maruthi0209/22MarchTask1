e6e9229fd9384d54dc9d1206fff0b452ceaed3d4
maruthi0209 <sethumaruthi93@gmail.com>
2025-03-22 17:03:04 +0530

    added spaces after labels

--- src/FormFields/loginForm.jsx
+++ src/FormFields/loginForm.jsx
@@ -3,7 +3,7 @@ import { fields } from "./fields"
 export const renderedloginfield = fields.loginForm.fieldProperties.map(function(value, index) {
     // console.log(value, index);
     return <div key={index} className={fields.loginForm.title + "inner"}>
-    <label htmlFor={value.id}>{fields.loginForm.labels[index]}</label>
+    <label htmlFor={value.id}>{fields.loginForm.labels[index] + ": "}</label>
     <input type={value.type} placeholder={value.placeholder} id={value.id} name={value.name} />
     </div>
   })
--- src/FormFields/registerForm.jsx
+++ src/FormFields/registerForm.jsx
@@ -3,7 +3,7 @@ import { fields } from "./fields"
 export const renderedpasswordfield = fields.registerForm.fieldProperties.map(function(value, index) {
     // console.log(value, index);
     return <div key={index} className={fields.registerForm.title+ "inner"}>
-    <label htmlFor={value.id}>{fields.registerForm.labels[index]}</label>
+    <label htmlFor={value.id}>{fields.registerForm.labels[index] + ": "}</label>
     <input type={value.type} placeholder={value.placeholder} id={value.id} name={value.name} />
     </div>
   })
